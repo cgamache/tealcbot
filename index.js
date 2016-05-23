83df4d87f6ad47f99866a704b86b0f8c8bef1127
Chris Gamache <cgamache@gmail.com>
2016-05-23 09:18:06 -0400

    Adding slight delay for response

--- index.js
+++ index.js
@@ -11,5 +11,7 @@ bot.startRTM(function(err,bot,payload) {
 
 controller.hears(["^yes$","^ya$","^si","indeed","affirmative","^vg$","^sure$","^def$","^definitely$","^agreed$","^roger$","^roger that$","^certainly$","^absolutely$",
 "^yep$","^yup$","^.+ right\\?$","^hm+","sweet$"],["direct_message","direct_mention","mention","ambient"],function(bot,message) {
-  bot.reply(message,'Indeed.');
+  setTimeout(function () { 
+	bot.reply(message,'Indeed.'); 
+  },Math.trunc((Math.random() * 2000)) + 1000);
 });
