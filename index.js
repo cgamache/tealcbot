327e003663148fb9e0824a3c51edfc33206341fb
Chris Gamache <cgamache@gmail.com>
2016-10-05 16:29:14 -0400

    Refactored. Added reconnect on disconnect.

--- index.js
+++ index.js
@@ -1,17 +1,42 @@
-var Botkit = require('botkit');
-var controller = Botkit.slackbot();
-var bot = controller.spawn({
-  token: process.env.SLACK_TOKEN
-})
-bot.startRTM(function(err,bot,payload) {
-  if (err) {
-    throw new Error('Could not connect to Slack');
-  }
-});
 
-controller.hears(["^yes$","^ya$","^si","indeed","affirmative","^vg$","^sure$","^def$","^definitely$","^agreed$","^roger$","^roger that$","^certainly$","^absolutely$",
-"^yep$","^yup$","^.+ right\\?$","^hm+","sweet$"],["direct_message","direct_mention","mention","ambient"],function(bot,message) {
+var utterances = ["^yes$","^ya$","^si","indeed","affirmative",
+	"^vg$","^sure$","^def$","^definitely$","^agreed$",
+	"^roger$","^roger that$","^certainly$","^absolutely$",
+	"^yep$","^yup$","^.+ right\\?$","^hm+","sweet$"];
+
+var Botkit = require('botkit');
+
+var controller = Botkit.slackbot();
+
+var init = function() {
+
+	var bot = controller.spawn({
+	  token: process.env.SLACK_TOKEN
+	});
+
+	bot.startRTM(function(err,bot,payload) {
+	  if (err) {
+	    throw new Error('Could not connect to Slack (' + err + ')');
+	  }
+	});
+
+	return bot;
+
+}
+
+var bot = init();
+
+controller.hears(utterances,["direct_message","direct_mention","mention","ambient"],function(bot,message) {
   setTimeout(function () { 
 	bot.reply(message,'Indeed.'); 
   },Math.trunc((Math.random() * 2000)) + 1000);
 });
+
+controller.on('rtm_close', function() {
+  console.log('Disconnected from RTM');
+  bot = init();
+});
+
+
+
+
